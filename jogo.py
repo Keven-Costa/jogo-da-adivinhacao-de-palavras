98218df20553ce7e30de9191ed14cb3a50f4d1b0
Keven Costa <ogkeven278@gmail.com>
2023-10-10 18:13:42 -0300

    first commit

---
+++ jogo.py
@@ -0,0 +1,7 @@
+import random
+
+palavras = ["ALGORITMO", "VARIÁVEL", "CONDICIONAL", "ITERAÇÃO", "FUNÇÃO", "ARRAY", "FRONTEND",
+            "COMPILAÇÃO", "INTERFACE", "OBJETO", "HERANÇA", "POLIMORFISMO", "DESENVOLVEDOR",
+            "LOOP", "BIBLIOTECA", "SINTAXE", "CLASSE", "MÉTODO", "COMPILADOR", "BACKEND"]
+palavra_oculta = random.choice(palavras)
+print(palavra_oculta)
